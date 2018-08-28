b3ebcf69a0b41af5976bdeb624668d6c5f97ae14
nady <nitin.da@hcl.com>
2018-08-29 02:08:48 +0530

    new app project

---
+++ src/main/java/com/tutorialpoint/mycoolapp/App.java
@@ -0,0 +1,13 @@
+package com.tutorialpoint.mycoolapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World of maven!!! I am new to it" );
+    }
+}
